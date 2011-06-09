28d5605546ed75253a89f1712312888ccd2377eb
Mia <github@miaridge.com>
2011-06-09 12:34:12 +0100

    first commit with lots to be done for hack4europe

---
+++ sharewhatyousee.php
@@ -0,0 +1,175 @@
+<?php
+/*
+Plugin Name: Share What You See
+Plugin URI: https://github.com/mialondon/Share-what-you-see
+Description: WordPress plugin to add results from Europeana search for interesting things seen in museums etc to a blogpost
+Version: 0.3
+Author: Mia Ridge, Owen Stephens
+License: GPL2
+
+*/
+
+/*
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
+
+/////////// set up activation and deactivation stuff
+register_activation_hook(__FILE__,'sharewhatyousee_install');
+
+function sharewhatyousee_install() {
+  // do stuff when installed
+  global $wp_version;
+  if (version_compare($wp_version, "3", "<")) {
+    deactivate_plugins(basename(__FILE__)); // deactivate plugin
+    wp_die("This plugin requires WordPress Version 3 or higher.");
+    // also requires mmg for the tables, or should test for existence of mmg main plugin
+    // also relies on curl so check for it 
+  } else {
+    // create the tables if mmg main plugin hasn't already been installed
+    // ### to do
+ }
+}
+
+
+register_deactivation_hook(__FILE__,'sharewhatyousee_uninstall');
+
+function sharewhatyousee_uninstall() {
+  // do stuff
+}
+
+
+/////////// set up option storing stuff
+// create array of options
+$sharewhatyousee_options_arr=array(
+  "sharewhatyousee_Europeana_API_key"=>'',
+  "sharewhatyousee_Europeana_API_URL"=>'',
+  );
+
+// store them
+update_option('sharewhatyousee_plugin_options',$sharewhatyousee_options_arr); 
+
+// get them
+$sharewhatyousee_options_arr = get_option('sharewhatyousee_plugin_options');
+
+// use them. 
+$sharewhatyousee_Europeana_API_key = $sharewhatyousee_options_arr["sharewhatyousee_Europeana_API_key"];
+$sharewhatyousee_Europeana_API_URL = $sharewhatyousee_options_arr["sharewhatyousee_Europeana_API_URL"];
+// end option array setup
+
+// required in WP 3 but not earlier?
+add_action('admin_menu', 'sharewhatyousee_plugin_menu');
+
+/////////// set up stuff for admin options pages
+// add submenu item to existing WP menu
+function sharewhatyousee_plugin_menu() {
+add_options_page('Share what you see settings page', 'Share what you see settings', 'manage_options', __FILE__, 'sharewhatyousee_settings_page');
+}
+
+// call register settings function before admin pages rendered
+add_action('admin_init', 'sharewhatyousee_register_settings');
+
+function sharewhatyousee_register_settings() {
+  // register settings - array, not individual
+  register_setting('sharewhatyousee-settings-group', 'sharewhatyousee_settings_values');
+}
+
+// write out the plugin options form. Form field name must match option name.
+// add other options here as necessary e.g. new API URLs or updated defaults
+function sharewhatyousee_settings_page() {
+  
+  if (!current_user_can('manage_options'))  {
+    wp_die( __('You do not have sufficient permissions to access this page.') );
+  }
+
+  ?>
+  <div>
+  <h2><?php _e('share what you see plugin options', 'sharewhatyousee-plugin') ?></h2>
+  <form method="post" action="options.php">
+  <?php settings_fields('sharewhatyousee-settings-group'); ?>
+
+  <?php _e('Europeana API key','sharewhatyousee-plugin') ?> 
+  
+  <?php sharewhatyousee_Europeana_API_key(); ?><br />
+
+  <?php _e('Europeana API search URL','sharewhatyousee-plugin') ?> 
+  
+  <?php sharewhatyousee_Europeana_API_URL(); ?><br />
+
+  <p class="submit"><input type="submit" class="button-primary" value=<?php _e('Save changes', 'sharewhatyousee-plugin') ?> /></p>
+  </form>
+  </div>
+  <?php
+}
+
+// get options from array and display as fields
+
+function sharewhatyousee_Europeana_API_URL() {
+  // load options array
+  $sharewhatyousee_options = get_option('sharewhatyousee_settings_values');
+  
+  $sharewhatyousee_Europeana_API_URL = $sharewhatyousee_options['sharewhatyousee_Europeana_API_URL'];
+  
+  // display form field
+  echo '<input type="text" name="sharewhatyousee_settings_values[sharewhatyousee_Europeana_API_URL]" 
+  value="'.esc_attr($sharewhatyousee_Europeana_API_URL).'" />';
+}
+
+function sharewhatyousee_Europeana_API_key() {
+  // load options array
+  $sharewhatyousee_options = get_option('sharewhatyousee_settings_values');
+  
+  $sharewhatyousee_Europeana_API_key = $sharewhatyousee_options['sharewhatyousee_Europeana_API_key'];
+  
+  // display form field
+  echo '<input type="text" name="sharewhatyousee_settings_values[sharewhatyousee_Europeana_API_key]" 
+  value="'.esc_attr($sharewhatyousee_Europeana_API_key).'" />';
+}
+
+
+/*
+ * set up shortcode Sample: [sharewhatyousee]
+ *
+ * Initial code only at this stage to get stuff onto github, will need to tweak form
+ * to use the right fields for SWYS
+ * 
+ */
+function ShareWhatYouSeeShortCode($atts, $content=null) {
+  
+  if(@is_file(ABSPATH.'/wp-content/plugins/sharewhatyousee/sharewhatyousee_functions.php')) {
+      include_once(ABSPATH.'/wp-content/plugins/sharewhatyousee/sharewhatyousee_functions.php'); 
+  }
+    
+  $search_terms = stripslashes($_POST['search_term']); // the free-text search field
+  $search_place = stripslashes($_POST['search_place']); // the free-text search field
+  $search_time = stripslashes($_POST['search_time']); // the free-text search field
+  $search_sources = 'Europeana'; // which target APIs are being searched? Can be set in form but for now fake it as Europeana; default should really be all (if it's blank)
+  
+  if(!empty($search_terms) || !empty($search_time) || !empty($search_place)) {
+    // process - deal with search, display results and import into db
+    echo '<p>Searching now...</p>';  
+    SWYSGetEuropeanaSearchResults($search_terms,$search_place,$search_time,'import',$search_sources);
+    } else {
+    // display search box and instructions
+    SWYSPrintSearchForm();
+  }
+
+}
+
+// Add the shortcode
+add_shortcode('sharewhatyousee', 'ShareWhatYouSeeShortCode');
+
+?>
---
+++ sharewhatyousee_functions.php
@@ -0,0 +1,247 @@
+<?php
+/*
+ * Functions for the Share What You See plugin
+ * 
+ * File information:
+ * Contains functions to query the APIs for the terms provided
+ * 
+ */
+
+/*
+ * Notes on import adaptations:
+ * 
+ * Notes for Europeana:
+ * Some of the fields are less clean than one would like, but apparently that's
+ * down to the data providers
+ * 
+ * Import written for: WordPress 3 for generic WordPress installs
+ *  
+ */
+
+/*
+ * Needs adapting for SWYS ###
+ * ### also not to use the Europeana enrichment fields cos they weren't reliable
+ * Was: Specialised search box for Tag Duel - asks for period (time) and place
+ * Values may be free-text or come from the OpenSearch term list [Ian]
+ * Might be nice to add optional free-text search if it's likely to return lots of results
+ */
+function SWYSPrintSearchBox() {
+  ?>
+  <h3>Search for something you saw...</h3><form method="post" action="">
+    
+  <label class="" for="search_time">Pick a time...</label>
+	<input name="search_time" size="30" tabindex="1" value="" id="search_time" autocomplete="on" type="text"><br />
+  <label class="" for="search_place">Pick a place...</label>
+	<input name="search_place" size="30" tabindex="1" value="" id="search_place" autocomplete="on" type="text"><br />
+  <label class="" for="search_term">And maybe a search term...</label>
+	<input name="search_term" size="30" tabindex="1" value="" id="search_term" autocomplete="on" type="text"><br />
+	  
+          <input type="submit" name="search" value="Go!" />
+
+          </form>
+
+  <?php
+}
+
+
+/*
+ * Will need to vary depending on the target API
+ * Inputs: $terms is the search terms given in the box, $mode is display or import
+ * Needs updating to use $wpdb methods
+ */
+function SWYSGetEuropeanaSearchResults($search_terms,$search_place,$search_time,$mode,$search_sources) {
+  
+  global $wpdb;
+  
+  $api_provider;
+  $type;
+  // get site options
+  $mmg_import_Europeana_API_key = $mmg_import_options_arr["mmg_import_Europeana_API_key"];
+  $mmg_import_Europeana_API_URL = $mmg_import_options_arr["mmg_import_Europeana_API_URL"];
+  
+  /* Some of this is based on stuff in January at CultureHackDay designed to get CultureGrid in without disturbing too much existing code. Insert moral tale about shortcuts eventually taking longer here...
+    ### Right now this needs to be updated to deal with search place and time (if they exist) for Europeana
+    But it also shouldn't be a case statement because it only allows for one provider search at a time
+  */
+  switch ($search_sources) {
+      case 'Europeana':
+          $type = 'xml';
+          $api_provider = 'Europeana'; // ### will presumably need to be the source provider? Check the field
+	  $url = 'http://api.europeana.eu/api/opensearch.rss' . '?searchTerms=';
+	  // the search seems to deal with extra '&' so am doing that rather than conditional crap
+	  if (!empty($search_terms)) {
+	    $url .=  $search_terms . '&';
+	  }
+	  if (!empty($search_time)) {
+	    $url .= 'enrichment_period_term:' . $search_time .'&';
+	  } 
+  	  if (!empty($search_place)) {
+	    $url .= 'enrichment_place_label:' . $search_place;
+	  }
+	  $url .= '&wskey=' . 'XADNJCFGME';
+	  // $url = $mmg_import_Europeana_API_URL . '?searchTerms=' . $search_terms . '&enrichment_period_term=' . $search_time . '&enrichment_place_label=' . $search_place . '&wskey=' . $mmg_import_Europeana_API_key; // ### not getting the settings?
+	  echo $url;
+	  // could also look for completeness (<europeana:completeness>) values 1 - 10
+          break;
+
+  }
+
+echo "<br />Loading list file for your search... <br />";
+
+  //echo $url;
+
+  $ch = curl_init(); // relies on curl
+  curl_setopt($ch, CURLOPT_USERAGENT, 'API client');      
+  curl_setopt($ch, CURLOPT_URL, $url); 
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  $output = curl_exec($ch); 
+  curl_close($ch);
+
+  if (!empty($output)) { 
+    switch ($type) {
+      case 'xml':
+	if ($search_sources == 'Europeana') {
+          $rss = simplexml_load_string($output);
+	  $data = $rss->channel;
+	  //$atom = $rss->channel;
+	  //$data = $atom->children('atom');
+	} else {
+          $data = simplexml_load_string($output);	  
+	}
+        break;
+      case 'json':
+        $data = json_decode($output,true);
+        break;
+    }
+  }
+
+  if (!empty($data)) {
+  
+    if ($mode == 'display') {
+    
+      echo '<ul>';
+      foreach($data->entry as $e){
+        echo '<li><a href="' . $e->link[0]['href'] . 
+             '">'.$e->title.'</a></li>';
+      }
+      echo '</ul>';
+      
+      //echo '<pre>';
+      //print_r($data);
+      //echo '</pre>';
+    
+    } else {
+  
+    // set up database stuff specific to the db table for this API (if it's not using the general object table) object_table
+    // $db_objecttable = 'wp_mmg_objects_powerhouse';
+    DEFINE("db_objecttable",table_prefix.'objects');
+    DEFINE("db_objecttablefields",'name, accession_number, institution, data_source_url, source_display_url, description, date_earliest, date_latest, interpretative_date, interpretative_place, image_url, subject_group, material_form');
+    
+    // process the file
+    // ### this needs to be updated for SWYS as not importing but displaying back to screen
+    
+    if( sizeof($data) > 0 ){
+
+      $i;
+    
+      echo 'Loading records into database...';    
+      switch ($search_sources) {
+	  case 'Europeana':
+	    $terms = 'Keyword='.$search_terms.'&Place='.$search_place.'&Time='.$search_time; // for reference later
+	    //echo '<pre>'.print_r($data).'</pre>';
+            $i = mmgDoEuropeanaImportXML($data,$terms);
+            break;
+      }
+      //echo "Building SQL string... ";
+  
+      echo '<p>'.$i.' objects loaded.</p>';
+        
+    } else {
+      echo 'Return size apparently < 0'; // April 2011 ###
+    }
+  }
+  } else {
+    echo '<br />No results found for that search term. Try again!';
+  }
+}
+
+function mmgDoEuropeanaImportXML($data,$terms) {
+  $i;
+  //echo 'hello';
+  // Iterate over each <item> in the channel element.
+  echo '<pre>'.print_r($data).'</pre>';
+  echo 'size: '.sizeof($data->item) ;
+    foreach($data->item as $item) {
+      echo 'in loop<br />';
+//  foreach($data->channel->item as $doc) {
+    // Now we need to fetch the URL identified in the <link> element
+    $ch = curl_init(); // relies on curl
+    curl_setopt($ch, CURLOPT_USERAGENT, 'API client');    
+//    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+    curl_setopt($ch, CURLOPT_URL, $doc->link);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $resultdoc = curl_exec($ch);
+    curl_close($ch);
+
+    // Check that the link element really returned us something
+    if ( !empty($resultdoc) ) {
+      // Parse what was returned
+      $xmldoc = simplexml_load_string($resultdoc);
+
+      // searchRetrieveResponse->records->record->recordData->dc->title
+      // Create an object based on that document
+      mmgInsertObject($xmldoc->records->record->recordData->dc->title,
+                      $xmldoc->records->record->recordData->dc->uri,
+                      $xmldoc->records->record->recordData->dc->publisher,  // Institutional Provider
+                      'http://api.europeana.org',  // Data source URL
+                      $xmldoc->records->record->recordData->dc->uri,
+                      $xmldoc->records->record->recordData->dc->description,
+                      "",  // Date_earliest
+                      "",  // Data_latest
+                      "", // $doc['dcterms.temporal'][0],  // interpretative_date
+                      "", // $doc['dcterms.spatial'][0],  // interpretative_place
+                      $xmldoc->records->record->recordData->dc->object, // 'http://culturegrid.org.uk' . $doc['cached_thumbnail'],  // image url
+                      $terms); // terms
+
+    }
+  }
+}
+
+function mmgInsertObject($object_name,$accession_number,$api_provider, $data_source_url, $source_display_url, $description, $date_earliest, $date_latest, $interpretative_date, $interpretative_place, $image_url, $terms) {
+  global $wpdb;
+  if (!empty($image_url)) {
+    // debug echo '<h2>in insert statement</h2>';
+    $sqlresult = $wpdb->query( $wpdb->prepare( "
+      INSERT IGNORE INTO ". db_objecttable . " 
+      (".db_objecttablefields.")
+      VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s )" ,
+      array( $object_name,$accession_number,$api_provider, $data_source_url, $source_display_url, $description, $date_earliest, $date_latest, $interpretative_place, $interpretative_place, $image_url, $terms ) ) ); 
+
+      if ( $sqlresult == 0 ) {
+        echo 'OK <br/>';
+      }
+      else { 
+        // echo 'Error : ' . mysql_errno() . ' ' . mysql_error() . $object_name . '<br/>';        
+        // echo $object_name . '-' . $accession_number . '-' . $api_provider . '-' .  $data_source_url. '-' .  $source_display_url . '-' .  $description . '-de:' .  $date_earliest . '-dl:' .  $date_latest. '-id:' .  $interpretative_date. '-ip:' .  $interpretative_place . '-' .  $image_url . '-' . $terms . '.<br/>';
+      }
+
+  }
+  else {
+    echo 'Item has no image....';
+  }
+}
+
+function getSingleValue($document,$xpath) {
+  $result;
+  $nodelist = $document->xpath($xpath);
+  echo "nl: $nodelist <br/>";
+  if ( count($nodelist) > 0 ) {
+    $result = $nodelist[0];
+  }
+  else {
+    $result = "";
+  }
+  return $result;
+}
+
+?>
